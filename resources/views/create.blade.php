8a2284c18b86028e0131b266b6f44b6b93a32c91
Roger Abucejo Jr <rogerhapay@gmail.com>
2024-07-15 17:24:26 +0800

    ongoing forms

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,9 +1,20 @@
 @extends('layouts.app')
-@section('title','Add Task')
+@section('title', 'Add Task')
 
 @section('content')
-    <form method="POST" action="{{route('tasks.store')}}">
+    <form method="POST" action="{{ route('tasks.store') }}">
         @csrf
-
+        <div>
+            <label for="title">Title</label>
+            <input type="text" name="title" id="title">
+        </div>
+        <div>
+            <label for="description">Description</label>
+            <textarea type="text" name="description" id="description" rows="5"></textarea>
+        </div>
+        <div>
+            <label for="long_description">Long Description</label>
+            <textarea type="text" name="long_description" id="long_description" rows="5"></textarea>
+        </div>
     </form>
 @endsection
