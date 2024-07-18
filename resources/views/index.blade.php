2c86400992febcd4d6df8f32f6149e58efa427e8
Roger Abucejo Jr <rogerhapay@gmail.com>
2024-07-18 14:21:48 +0800

    add delete button

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,17 @@
 @extends('layouts.app')
 @section('title', 'Task List')
 @section('content')
-@forelse($tasks as $task)
-    <div>
-        <a href="{{ route('tasks.showPerId', ['id' => $task->id]) }}">{{ $task->title }}</a>
-    </div>
-@empty
-    <div>No tasks</div>
-@endforelse
+    @forelse($tasks as $task)
+        <div>
+            <a href="{{ route('tasks.showPerId', ['id' => $task->id]) }}">{{ $task->title }}</a>
+        </div>
+    @empty
+        <div>No tasks</div>
+    @endforelse
+
+    {{-- @if ($task->count())
+        <nav> --}}
+    {{ $task->links() }}
+    {{-- </nav> --}}
+    {{-- @endif --}}
 @endsection
