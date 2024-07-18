fd915c8990008db7e2ea75febb2f14c7726f9055
Roger Abucejo Jr <rogerhapay@gmail.com>
2024-07-18 14:21:26 +0800

    add delete button

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -7,4 +7,12 @@
     @endif
     <p>{{ $task->created_at }}</p>
     <p>{{ $task->updated_at }}</p>
+
+    <div>
+        <form action="{{route('tasks.destroy',['id'=> $task->id])}}" method="post">
+            @csrf
+            @method('DELETE')
+            <button type="submit">Delete</button>
+        </form>
+    </div>
 @endsection
